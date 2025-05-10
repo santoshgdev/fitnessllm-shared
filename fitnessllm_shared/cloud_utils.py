d69b7c2c4df35d47ac069ec386b64268b2b7cad3
santoshgdev <santosh@santoshg.io>
2025-05-09 21:04:35 -0700

    update

---
+++ fitnessllm_shared/cloud_utils.py
@@ -0,0 +1,32 @@
+"""Cloud utils."""
+import json
+from os import environ
+
+from beartype import beartype
+from google.cloud import secretmanager
+
+from fitnessllm_shared.logger_utils import create_structured_logger
+
+structured_logger = create_structured_logger(__name__)
+
+
+@beartype
+def get_secret(name: str) -> dict:
+    """Retrieve secret from secret manager."""
+    if "PROJECT_ID" not in environ:
+        raise KeyError("PROJECT_ID environment variable not found")
+    structured_logger(message="Initializing secret manager")
+    client = secretmanager.SecretManagerServiceClient()
+    structured_logger(message=f"Getting secret for {name}")
+    try:
+        response = client.access_secret_version(
+            request={
+                "name": f"projects/{environ['PROJECT_ID']}/secrets/{name}/versions/latest",
+            }
+        )
+        structured_logger(message=f"Retrieved secret {name}")
+        secret_payload = response.payload.data.decode("UTF-8")
+        return json.loads(secret_payload)
+    except Exception as e:
+        structured_logger(message=f"Failed to retrieve or decode secret {name}: {e}")
+        raise
